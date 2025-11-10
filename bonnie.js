1e30c48b9b4825c4f9a5052dca4320ca004921b2
Jake <privatekodrp@gmail.com>
2025-11-10 02:20:48 +0100

    Add files via upload
    bonnie jumpscare with js

---
+++ bonnie.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => { // wartet bis seite geladen ist
+const bonnieImg = document.getElementById('bonnie-img');
+const flashOverlay = document.getElementById('flash-overlay');
+const jumpscareOverlay = document.getElementById('jumpscare-overlay');
+const jumpScareSound = new Audio('bonnie.mp3');
+let isJumpscaring = false; // Damits nd durchgehend ballert
+bonnieImg.addEventListener('mouseover', () => {
+    if (isJumpscaring) return; // Damits sichs nd wiederholt
+    isJumpscaring = true; // Aktiv
+    jumpscareOverlay.style.display = 'flex'; // zeigt jumpscare bild
+    jumpScareSound.play();
+    flashOverlay.classList.add('flash-active');
+    setTimeout(() => {
+        flashOverlay.classList.remove('flash-active'); // macht flash nach 0.3s aus
+    }, 300);
+    setTimeout(() => {
+        jumpscareOverlay.style.display = 'none'; // macht jumpscare weg nach 1s
+        jumpScareSound.pause(); //pausiert den sound
+        jumpScareSound.currentTime = 0; // setzt den sound zurueck
+        isJumpscaring = false; // ready fuer naechsten jumpscare
+    }, 1000);
+});
+});
